62b42b27c60e0e89d3855d46ea457d10fefef9cd
Julian.GarzonA <Julian.GarzonA@perficient.com>
2021-11-11 14:14:51 -0500

    no message

--- src/index.js
+++ src/index.js
@@ -1,42 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const events = require('./events');
-const vendors = require('./vendors');
-const forbidden = require('./forbidden');
-const winners = require('./winners');
-const limit = require('./limit');
-const users = require('./users');
 
+const app = express();
 
-const connection = mysql.createConnection({
-  host     : 'remotemysql.com',
-  port     : '3306',
-  user     : 'YflbgGGuYI',
-  password : 'PRVwXgemLr',
-  database : 'YflbgGGuYI'
-});
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/bet'));
 
-connection.connect();
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/bet/'}),
+);
 
-const port = process.env.PORT || 8080;
-
-const app = express()
-  .use(express.static('./dist/bet'))
-  .use(cors())
-  .use(bodyParser.json())
-  .use(events(connection))
-  .use(vendors(connection))
-  .use(forbidden(connection))
-  .use(winners(connection))
-  .use(limit(connection))
-  .use(users(connection));
-
-app.get('/*', (req, res) => {
-  res.sendFile('index.html',{root: 'dist/bet/'});
-});
-app.listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
