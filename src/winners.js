b2a302181361efee8565ca07c85324292d7ba088
Julian.GarzonA <Julian.GarzonA@perficient.com>
2021-11-13 23:08:36 -0500

    Winner

--- src/winners.js
+++ src/winners.js
@@ -7,8 +7,8 @@ function createRouter(db) {
   
   router.get('/winners/:number/:date/:loteryCode', function (req, res, next) {      
     db.query(
-      'SELECT number, l.name, vendor_code, b.value, b.type, b.DATE, b.USERNAME FROM bets b INNER JOIN lotery l on b.lotery_code = l.code where (number = ? or substr(?,1,3) = number or substr(?,2,4) = number OR substr(?,3,4) = number) AND DATE_FORMAT(date, "%Y-%m-%d")= ? AND b.lotery_code = ?',
-      [req.params.number, req.params.number, req.params.number, req.params.number, req.params.date, req.params.loteryCode],
+      'SELECT number, l.name, vendor_code, b.value, b.type, b.DATE, b.USERNAME FROM bets b INNER JOIN lotery l on b.lotery_code = l.code where (number = ? or substr(?,2,4) = number OR substr(?,3,4) = number) AND DATE_FORMAT(date, "%Y-%m-%d")= ? AND b.lotery_code = ?',
+      [req.params.number, req.params.number, req.params.number, req.params.date, req.params.loteryCode],
       (error, results) => {
         if (error) {
           console.log(error);
