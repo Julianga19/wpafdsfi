c1ea7c242833998df476b3b1274e6afc963834e5
Julian.GarzonA <Julian.GarzonA@perficient.com>
2021-11-22 23:11:17 -0500

    Date limit

--- src/limit.js
+++ src/limit.js
@@ -21,9 +21,7 @@ function createRouter(db) {
   });
 
 
-  router.get('/limit/:value/:date/:limit/:today/:loteries', function (req, res, next) {                       
-    console.log(req.params.date);
-    console.log(req.params.today);
+  router.get('/limit/:value/:date/:limit/:today/:loteries', function (req, res, next) {                           
     db.query(
       'SELECT * FROM (SELECT b.NUMBER , b.lotery_code, l.NAME,SUM(VALUE) AS SUMA, (SELECT COALESCE(SUM(c.VALUE),0) FROM covered c WHERE b.number = c.number and b.LOTERY_CODE = c.LOTERY_CODE AND DATE_FORMAT(c.date,"%Y-%m-%d")=?) as COVERED FROM bets b INNER JOIN lotery l on b.lotery_code = l.code WHERE DATE_FORMAT(b.date, "%Y-%m-%d")= ? AND b.type ="Derecho" AND b.lotery_code in (' + req.params.loteries +') GROUP BY LOTERY_CODE, NAME, b.number) AS TAB WHERE SUMA >= ? AND LENGTH(NUMBER)= ? AND SUMA-COVERED-? >= 500 ',
       [req.params.today,req.params.date,req.params.value, req.params.limit, req.params.value],
